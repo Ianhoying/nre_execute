9d86899556a6be0b47308d1bca775140ce7b61b7
Ianhoying <145854610+Ianhoying@users.noreply.github.com>
2025-03-31 21:16:50 +0900

    Update nre_execute.py

--- nre_execute.py
+++ nre_execute.py
@@ -9,9 +9,9 @@ st.write("GitHub에서 최신 크롤링 코드를 가져와 실행합니다.")
 if st.button("크롤링 실행하기"):
     st.write("✅ GitHub에서 최신 코드 가져오는 중...")
     
-#     # GitHub에서 최신 코드 가져오기 (로컬 Git 저장소가 있어야 함)
-#     result = subprocess.run(["git", "pull"], capture_output=True, text=True)
-#     st.text(result.stdout)
+    # GitHub에서 최신 코드 가져오기 (로컬 Git 저장소가 있어야 함)
+    result = subprocess.run(["git", "pull"], capture_output=True, text=True)
+    st.text(result.stdout)
     
 #     # 크롤링 코드 실행 (Python 스크립트 실행)
 #     st.write("🛠 크롤링 실행 중...")
